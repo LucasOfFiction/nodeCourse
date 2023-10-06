559efe19b65f4c4e0b34e6c29f6182ba30d7641d
LucasOfFiction <lucas.2003.566@gmail.com>
2023-10-05 23:25:26 -0300

    start mysql model

--- clase4/app.js
+++ clase4/app.js
@@ -1,17 +1,12 @@
-import express, { json } from 'express' // require -> commonJS
+import express, { json } from 'express'
 import { moviesRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
-//los import de un json en el futuro se van a poder hacer así:
-//import movies from './movies.json' with {type: 'json'}
 
 const app = express()
 app.use(json())
 app.use(corsMiddleware())
-app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
-// CORS PRE-Flight
-// OPTIONS
+app.disable('x-powered-by') 
+
 app.use('/movies', moviesRouter)
 const PORT = process.env.PORT ?? 1234
 app.listen(PORT, () => {
