c7b99867d9d47b63500e18717807db9f57e9af93
LucasOfFiction <lucas.2003.566@gmail.com>
2023-11-16 20:52:26 -0300

    chat en tiempo real

---
+++ clase6/server/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hola mundo, esto es un chat</h1>')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port: ${PORT}`)
+})
