a1770462d2c0ef90a2e39de448d6bf08ad8150b9
LucasOfFiction <lucas.2003.566@gmail.com>
2023-11-22 17:45:18 -0300

    chat online

--- clase6/server/index.js
+++ clase6/server/index.js
@@ -1,12 +1,35 @@
 import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 
 const PORT = process.env.PORT ?? 3000
-const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hola mundo, esto es un chat</h1>')
+const app = express()
+const server = createServer(app)
+const io = new Server(server,{
+    connectionStateRecovery: {}
 })
 
-app.listen(PORT, ()=>{
+io.on('connection', (socket) => {
+    console.log('new connection')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html') //current work diorectory, donde se esta ejecutando el script
+})
+
+server.listen(PORT, ()=>{
     console.log(`server running on port: ${PORT}`)
 })
