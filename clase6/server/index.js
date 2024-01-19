b06deac15ca5e4369f179a8eb583751fa3187e0c
LucasOfFiction <lucas.2003.566@gmail.com>
2024-01-18 21:13:58 -0300

    este proyecto recién empieza

--- clase6/server/index.js
+++ clase6/server/index.js
@@ -6,6 +6,8 @@ import {createClient} from '@libsql/client'
 import { Server } from 'socket.io'
 import { createServer } from 'http'
 
+dotenv.config()
+
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
@@ -22,19 +24,57 @@ const db = createClient({
 await db.execute(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
     )
 `)
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('new connection')
 
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        const username= socket.handshake.auth.username ?? 'anonymous'
+        console.log({ username })
+
+        try {
+
+            result= await db.execute({
+                sql:'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+                args: {msg, username}
+            })
+        } catch (error) {
+            console.error(error)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+
+
+    console.log('auth')
+    console.log(socket.handshake.auth)
+
+    if(!socket.recovered){ // recupera los mensajes sin conexión
+        try {
+            const results = await db.execute({
+            sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+            args:[socket.handshake.auth.serverOffset ?? 0]
+        })
+
+        results.rows.forEach(row => {
+            socket.emit('chat message', row.content, row.id.toString(), row.user)
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
+
+
+    }
 })
 
 app.use(logger('dev'))
