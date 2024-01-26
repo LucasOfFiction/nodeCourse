f1d49ff07edf9540b451ee2263024cfb241281b2
LucasOfFiction <lucas.2003.566@gmail.com>
2024-01-26 12:56:09 -0300

    incorporación de LOGIN

--- clase7LOGIN/src/controllers/auth.controller.js
+++ clase7LOGIN/src/controllers/auth.controller.js
@@ -39,4 +39,31 @@ export const register = async (req, res) => {
 
 }
 
-export const login = (req, res) => res.send('login')
+export const login = async (req, res) => {
+    
+    const {email, password} = req.body
+
+    try {
+
+        const userFound = await User.findOne({email})
+        if(!userFound) return res.status(400).json({message: 'User not found'})
+        
+        const passwordMatch = await bcrypt.compare(password, userFound.password)  
+        if(!passwordMatch) return res.status(400).json({message: 'Invalid password'})
+
+        const token = await createAccessToken({id:userFound._id})
+        res.cookie('token', token)
+        res.json({
+           id: userFound._id,
+           username: userFound.username,
+           email: userFound.email,
+           createdAt: userFound.createdAt,
+           updatedAt: userFound.updatedAt
+        })
+        console.log(userFound)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+    
+}
