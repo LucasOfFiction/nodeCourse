20445b36e7ee70a118354cc1dc0ba0a4a5c7c3e1
LucasOfFiction <lucas.2003.566@gmail.com>
2024-01-25 21:47:53 -0300

    autenticación jwt

--- clase7LOGIN/src/controllers/auth.controller.js
+++ clase7LOGIN/src/controllers/auth.controller.js
@@ -1,25 +1,40 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs' 
+import { createAccessToken } from '../libs/jwt.js'
+
+
 
 export const register = async (req, res) => {
 
     const {email, password, username} = req.body
 
+    
     try {
+        
+        const passwordHash = await bcrypt.hash(password, 10)
 
         const newUser = new User({
             email,
-            password,
-            username
+            password: passwordHash,
+            username,
+            createdAt: new Date(),
+            updatedAt: new Date()
         })
 
         const userSaved = await newUser.save()
-        res.json(userSaved)
 
+        const token = await createAccessToken({id:userSaved._id})
+        res.cookie('token', token)
+        res.json({
+           id: userSaved._id,
+           username: userSaved.username,
+           email: userSaved.email,
+           createdAt: userSaved.createdAt,
+           updatedAt: userSaved.updatedAt
+        })
         console.log(userSaved)
-        //res.send('register successfull')
-
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }
 
 }
--- clase7LOGIN/src/models/user.model.js
+++ clase7LOGIN/src/models/user.model.js
@@ -16,6 +16,14 @@ const userSchema = new mongoose.Schema({
         type:String,
         required: true,
     },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now,
+    }
 })
 
 export default mongoose.model('User', userSchema)
