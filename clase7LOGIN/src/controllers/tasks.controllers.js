b29c944c220f9ce0aaa24e1ae3aa5556ef49563e
LucasOfFiction <lucas.2003.566@gmail.com>
2024-01-29 08:57:30 -0300

    CRUD NOTES APP

---
+++ clase7LOGIN/src/controllers/tasks.controllers.js
@@ -0,0 +1,22 @@
+import User from '../models/user.model.js'
+import taskSchema from '../models/task.model.js'
+
+
+
+export const getTasks = async (req, res) => {}
+
+
+
+export const createTask = async (req, res) => {}
+
+
+
+export const getTask = async (req, res) => {}
+
+
+
+export const deleteTask = async (req, res) => {}
+
+
+
+export const updateTask = async (req, res) => {}
