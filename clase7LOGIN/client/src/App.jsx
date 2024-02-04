dc8f3768cc8002a4ff391c942fa936e8ebd4d2d9
LucasOfFiction <lucas.2003.566@gmail.com>
2024-02-03 22:29:09 -0300

    registerPage

--- clase7LOGIN/client/src/App.jsx
+++ clase7LOGIN/client/src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import RegisterPage from "./pages/registerPage"
 import LoginPage from "./pages/LoginPage"
+import { AuthProvider } from "./context/authContext"
+import RegisterPage from "./pages/RegisterPage"
 
 function App(){
 
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1 className="text-4xl font-bold">Home Page</h1>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/tasks" element={<h1>tasks</h1>}/>
-        <Route path="/add-tasks" element={<h1>add task</h1>}/>
-        <Route path="/tasks/:id" element={<h1>task</h1>}/>
-        <Route path="/Profile" element={<h1>Profile</h1>}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1 className="text-4xl font-bold">Home Page</h1>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/tasks" element={<h1>tasks</h1>}/>
+          <Route path="/add-tasks" element={<h1>add task</h1>}/>
+          <Route path="/tasks/:id" element={<h1>task</h1>}/>
+          <Route path="/Profile" element={<h1>Profile</h1>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- clase7LOGIN/client/src/pages/RegisterPage.jsx
+++ clase7LOGIN/client/src/pages/RegisterPage.jsx
@@ -1,15 +1,35 @@
 import {useForm} from 'react-hook-form'
-import { registerRequest } from '../api/auth'
-
+import { useAuth } from '../context/authContext'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function RegisterPage() {
-    const {register, handleSubmit} = useForm()
-    const onSubmit = handleSubmit ( async (values)=> {
-        const res = await registerRequest(values)
-        console.log(res)})
+    const {
+        register,
+        handleSubmit,
+        formState:{errors},
+    } = useForm()
+    const {signup, isAuthenticated, errors: registerErrors} = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/tasks')
+        }
+    }, [isAuthenticated])
+
+
+    const onSubmit = handleSubmit ( async (values)=> {signup(values)})
+
     return(
         <div className='bg-zinc-900 max-w-md p-10 rounded-md'>
-            
+            {
+                registerErrors.map((error, i)=>(
+                    <div className='bg-red-500 p-2 text-white' key={i}>
+                        {error}
+                    </div>
+                ))
+            }
             {/* en lugar de estilizar los elementos por separado puedo transformalos en un componenete reutilizable, investigar más para poder hacerlo */}
             
             <form onSubmit={onSubmit}>
@@ -17,14 +37,17 @@ function RegisterPage() {
                 className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
                 placeholder='Username'
                 />   
+                {errors.username && <p className='text-red-500'>Username is required</p>}
                 <input type="email" {...register('email', {required:true})}
                 className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
                 placeholder='Email'
                 />
+                {errors.email && <p className='text-red-500'>Email is required</p>}
                 <input type="password" {...register('password', {required:true})}
                 className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
                 placeholder='Password'
                 />
+                {errors.password && <p className='text-red-500'>Password is required</p>}
                 <button type='submit'
                 className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                 >
