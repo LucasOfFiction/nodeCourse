31598b33c1e6125945c415d83ea3cc120c67c2cb
LucasOfFiction <lucas.2003.566@gmail.com>
2024-02-05 13:54:49 -0300

    LOGIN

--- clase7LOGIN/client/src/pages/RegisterPage.jsx
+++ clase7LOGIN/client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import {useForm} from 'react-hook-form'
 import { useAuth } from '../context/authContext'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 function RegisterPage() {
     const {
@@ -16,45 +16,53 @@ function RegisterPage() {
         if (isAuthenticated) {
             navigate('/tasks')
         }
-    }, [isAuthenticated])
+    }, [isAuthenticated, navigate])
 
 
     const onSubmit = handleSubmit ( async (values)=> {signup(values)})
 
     return(
-        <div className='bg-zinc-900 max-w-md p-10 rounded-md'>
-            {
-                registerErrors.map((error, i)=>(
-                    <div className='bg-red-500 p-2 text-white' key={i}>
-                        {error}
-                    </div>
-                ))
-            }
-            {/* en lugar de estilizar los elementos por separado puedo transformalos en un componenete reutilizable, investigar más para poder hacerlo */}
-            
-            <form onSubmit={onSubmit}>
-                <input type="text" {...register('username',{required:true})}
-                className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
-                placeholder='Username'
-                />   
-                {errors.username && <p className='text-red-500'>Username is required</p>}
-                <input type="email" {...register('email', {required:true})}
-                className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
-                placeholder='Email'
-                />
-                {errors.email && <p className='text-red-500'>Email is required</p>}
-                <input type="password" {...register('password', {required:true})}
-                className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
-                placeholder='Password'
-                />
-                {errors.password && <p className='text-red-500'>Password is required</p>}
-                <button type='submit'
-                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                >
-                    REGISTER
-                </button>
-            </form>
-        </div>
+        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+            <div className='bg-zinc-900 max-w-md p-10 rounded-md'>
+                {
+                    registerErrors.map((error, i)=>(
+                        <div className='bg-red-500 p-2 text-white' key={i}>
+                            {error}
+                        </div>
+                    ))
+                }
+                {/* en lugar de estilizar los elementos por separado puedo transformalos en un componenete reutilizable, investigar más para poder hacerlo */}
+                
+                <h1 className="text-2xl font-bold">Register</h1>
+
+                <form onSubmit={onSubmit}>
+                    <input type="text" {...register('username',{required:true})}
+                    className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='Username'
+                    />   
+                    {errors.username && <p className='text-red-500'>Username is required</p>}
+                    <input type="email" {...register('email', {required:true})}
+                    className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='Email'
+                    />
+                    {errors.email && <p className='text-red-500'>Email is required</p>}
+                    <input type="password" {...register('password', {required:true})}
+                    className='w-full bg-zinc-800 text-white px-4 py-2 rounded-md my-2'
+                    placeholder='Password'
+                    />
+                    {errors.password && <p className='text-red-500'>Password is required</p>}
+                    <button type='submit'
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                    >
+                        REGISTER
+                    </button>
+                </form>
+
+                <p className='flex gap-x-2'>Already have an account? <Link to='/login' className='text-blue-500'>Sign in</Link></p>
+
+
+            </div>
+        </div>    
     )
         
     }
--- clase7LOGIN/src/controllers/auth.controller.js
+++ clase7LOGIN/src/controllers/auth.controller.js
@@ -6,12 +6,12 @@ import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
 
-    const {email, password, username} = req.body
+    const { username, password} = req.body
 
     
     try {
-        const userFound = await User.findOne({email})
-        if(userFound) return res.status(400).json(['The email is already in use'])
+        const userFound = await User.findOne({username})
+        if(userFound) return res.status(400).json(['The username is already in use'])
         
         const passwordHash = await bcrypt.hash(password, 10)
 
